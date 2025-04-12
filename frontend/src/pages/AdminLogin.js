5242387f6bec5eec201707eb5f7eb94171abb339
insearchwhy <156780681+insearchwhy@users.noreply.github.com>
2025-04-12 12:02:21 +0530

    change to dynamic url

--- frontend/src/pages/AdminLogin.js
+++ frontend/src/pages/AdminLogin.js
@@ -13,7 +13,7 @@ const AdminLogin = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post('http://localhost:5000/api/auth/login', {
+            const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
                 email,
                 password
             });
